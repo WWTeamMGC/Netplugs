6f52764dd91cb151b27df4b70d62cf0e6b13a0ed
LILILIBIU <1710555103@qq.com>
2022-07-04 12:37:45 +0800

    delete something

--- main.go
+++ main.go
@@ -22,21 +22,5 @@ func main() {
 		params := c.Param("name")
 		c.JSON(200, gin.H{"Hello": 200, "name": params})
 	})
-	r.Run(":8081")
-	//url := "http://127.0.0.1:8080/BadApi/Ip"
-	//http.Client{
-	//	Transport:     nil,
-	//	CheckRedirect: nil,
-	//	Jar:           nil,
-	//	Timeout:       0,
-	//}
-	//type T struct {
-	//	Badiplist []string `json:"badiplist"`
-	//}
-	//BadIPList := &T{}
-	//s, err := req.Post(url)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//json.Unmarshal(s.Bytes(), &BadIPList)
+	r.Run(":8089")
 }
