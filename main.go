55c8f470bba67208cea7845d7edc7fd163b887a5
LILILIBIU <1710555103@qq.com>
2022-07-06 15:06:11 +0800

    test

--- main.go
+++ main.go
@@ -13,14 +13,37 @@ import (
 func main() {
 	r := gin.Default()
 	config := &Config.SetConfig{
-		SetIPFilter:     true,
-		SetWordsFilter:  true,
+		SetIPFilter:     false,
+		SetWordsFilter:  false,
 		SetImagesFilter: false,
 	}
 	driver.InitServer(config)
-	r.GET("/hello", Netplugs.NetGinPlug(config), func(c *gin.Context) {
+	r.Use(Netplugs.NetGinPlug(config))
+	r.GET("/hello", func(c *gin.Context) {
 		params := c.Param("name")
 		c.JSON(200, gin.H{"Hello": 200, "name": params})
 	})
-	r.Run(":8089")
+	r.GET("/shop/cat", func(c *gin.Context) {
+		params := c.Param("name")
+		c.JSON(200, gin.H{"Hello": 200, "name": params})
+	})
+	r.GET("/shop/dog", func(c *gin.Context) {
+		params := c.Param("name")
+		c.JSON(200, gin.H{"Hello": 200, "name": params})
+	})
+
+	r.GET("/pay", func(c *gin.Context) {
+		params := c.Param("name")
+		c.JSON(200, gin.H{"Hello": 200, "name": params})
+	})
+	r.GET("/study/english", func(c *gin.Context) {
+		params := c.Param("name")
+		c.JSON(200, gin.H{"Hello": 200, "name": params})
+	})
+	r.GET("/study/math", func(c *gin.Context) {
+		params := c.Param("name")
+		c.JSON(200, gin.H{"Hello": 200, "name": params})
+	})
+
+	r.Run(":8085")
 }
