83ac7373cdd49fe6776a80831abdce9fcf1eb854
LILILIBIU <1710555103@qq.com>
2022-07-06 13:23:43 +0800

    fix bugs

--- model/HttpInfo.go
+++ model/HttpInfo.go
@@ -11,5 +11,10 @@ type Badwordslist struct {
 	Badwordslist []string `json:"badwordslist"`
 }
 type Badiplist struct {
-	Badiplist []string `json:"badiplist"`
+	Badiplist []BadIPListRsp `json:"badiplist"`
+}
+type BadIPListRsp struct {
+	Ip      string `json:"ip"`
+	PcMp    string `json:"pc_mp"`
+	Address string `json:"address"`
 }
